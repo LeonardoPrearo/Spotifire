51b2a7f07c6c3e63d45253c19cbf88ab40fc2a08
martinabove <marti_bove13@libero.it>
2022-06-15 17:59:34 +0200

    finito first day

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {LayoutModule} from "./core/layout/layout.module";
+import {FooterModule} from "./core/layout/footer/footer.module";
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    LayoutModule,
+    FooterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/core/model/song.ts
@@ -0,0 +1,9 @@
+export interface Song{
+  id : number,
+  artist: string,
+  title : string,
+  duration : number,
+  img : string
+}
+
+export type Songs = Song[]
